874d2ef44f4197c7e2868d17034816a85c594993
jananikangesan <janakanges1996@gmail.com>
2025-03-23 14:32:05 +0530

    swagger document added in http://localhost:8085/swagger-ui/index.html

--- src/main/java/com/todo/demo/controller/TaskController.java
+++ src/main/java/com/todo/demo/controller/TaskController.java
@@ -2,6 +2,8 @@ package com.todo.demo.controller;
 
 import com.todo.demo.model.Task;
 import com.todo.demo.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,14 @@ public class TaskController {
     private TaskService taskService;
 
     //fetch task from external API
+    @Operation(
+            summary = "Fetch tasks from external API",
+            description = "Retrieve a list of tasks from an external API.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully fetched tasks"),
+                    @ApiResponse(responseCode = "204", description = "No content, no tasks found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @GetMapping("/all")
     public ResponseEntity<List<Task>> getTasksFromAPI() {
         try {
@@ -32,6 +42,13 @@ public class TaskController {
     }
 
     //fetch task from external API and store it into database
+    @Operation(
+            summary = "Fetch and store tasks",
+            description = "Fetch tasks from an external API and store them in the database.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Tasks successfully fetched and stored"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @GetMapping("/fetch-and-store")
     public ResponseEntity<String> fetchAndStoreTasks() {
         try {
@@ -46,6 +63,14 @@ public class TaskController {
     }
 
     //Get all stored tasks from database
+    @Operation(
+            summary = "Get all tasks",
+            description = "Retrieve all tasks that are stored in the database.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully fetched tasks"),
+                    @ApiResponse(responseCode = "204", description = "No content, no tasks found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @GetMapping("/")
     public ResponseEntity<List<Task>> getAllTasks(){
         try {
@@ -60,6 +85,14 @@ public class TaskController {
     }
 
     //Get task by ID to retrieve task details from the database.
+    @Operation(
+            summary = "Get task by ID",
+            description = "Retrieve a task from the database by its ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully fetched the task"),
+                    @ApiResponse(responseCode = "404", description = "Task not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
         Task task = taskService.getTaskById(id);
@@ -67,6 +100,14 @@ public class TaskController {
     }
 
     //Update task details using an ID.
+    @Operation(
+            summary = "Update task details",
+            description = "Update the details of an existing task by its ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully updated the task"),
+                    @ApiResponse(responseCode = "404", description = "Task not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
         Task updated = taskService.updateTask(id, updatedTask);
@@ -74,6 +115,14 @@ public class TaskController {
     }
 
     //Delete a task from the database using an ID.
+    @Operation(
+            summary = "Delete a task",
+            description = "Delete a task from the database using its ID.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully deleted the task"),
+                    @ApiResponse(responseCode = "404", description = "Task not found"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteTask(@PathVariable Long id) {
 
@@ -81,6 +130,13 @@ public class TaskController {
     }
 
     //Get grouped tasks by completion status to retrieve tasks grouped into completed and pending categories
+    @Operation(
+            summary = "Get grouped tasks by completion status",
+            description = "Retrieve tasks grouped into completed and pending categories based on their completion status.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Successfully grouped tasks by completion status"),
+                    @ApiResponse(responseCode = "500", description = "Internal server error")
+            })
     @GetMapping("/grouped")
     public Map<Boolean, List<Task>> getGroupedTasks() {
         return taskService.getGroupedTasksByCompletionStatus();
